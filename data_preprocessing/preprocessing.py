cec88411dd1eb3390f6acc75de9142b37a5de5f8
Sujan Kumar Patro <sujanpatro@gmail.com>
2022-05-20 21:23:46 +0530

    preprocessing structure defined

--- data_preprocessing/preprocessing.py
+++ data_preprocessing/preprocessing.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn_pandas import CategoricalImputer
+from imblearn.over_sampling import RandomOverSampler
 
 
 class Preprocessor:
     def __init__(self, file_object, logger_object):
-        self.useful_data = None
-        self.columns = None
-        self.df_without_spaces = None
-        self.data = None
         self.file_object = file_object
         self.logger_object = logger_object
 
@@ -41,4 +39,109 @@ class Preprocessor:
             raise e
 
     def separate_label_feature(self, data, label_column_name):
-        pass
+        self.logger_object.log(self.file_object, 'Entered the separate_label_feature method of the Preprocessor class')
+        try:
+            self.X = data.drop(labels=label_column_name,
+                               axis=1)  # drop the columns specified and separate the feature columns
+            self.Y = data[label_column_name]  # Filter the Label columns
+            self.logger_object.log(self.file_object,
+                                   'Label Separation Successful. Exited the separate_label_feature method of the Preprocessor class')
+            return self.X, self.Y
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occured in separate_label_feature method of the Preprocessor class. Exited method. %s' % e)
+            raise e
+
+    def is_null_present(self, data):
+        self.logger_object.log(self.file_object, 'Entered the is_null_present method of the Preprocessor class')
+        self.null_present = False
+        self.cols_with_missing_values = []
+        self.cols = data.columns
+
+        try:
+            self.null_counts = data.isna().sum() # check for the count of null values per column
+            for i in range(len(self.null_counts)):
+                if self.null_counts[i]>0:
+                    self.null_present=True
+                    self.cols_with_missing_values.append(self.cols[i])
+            if(self.null_present): # write the logs to see which columns have null values
+                self.dataframe_with_null = pd.DataFrame()
+                self.dataframe_with_null['columns'] = data.columns
+                self.dataframe_with_null['missing values count'] = np.asarray(data.isna().sum())
+                self.dataframe_with_null.to_csv('preprocessing_data/null_values.csv') # storing the null column information to file
+            self.logger_object.log(self.file_object,'Finding missing values is a success. Data written to the null values file. Exited the is_null_present method of the Preprocessor class')
+            return self.null_present, self.cols_with_missing_values
+
+        except Exception as e:
+            self.logger_object.log(self.file_object,'Exception occured in is_null_present method of the Preprocessor class. Exited method. %s' % e)
+            raise e
+
+    def impute_missing_values(self, data, cols_with_missing_values):
+        self.logger_object.log(self.file_object, 'Entered the impute_missing_values method of the Preprocessor class')
+        self.data = data
+        self.cols_with_missing_values = cols_with_missing_values
+        try:
+            self.imputer = CategoricalImputer()
+            for col in self.cols_with_missing_values:
+                self.data[col] = self.imputer.fit_transform(self.data[col])
+            self.logger_object.log(self.file_object,
+                                   'Imputing missing values Successful. Exited the impute_missing_values method of the Preprocessor class')
+            return self.data
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occured in impute_missing_values method of the Preprocessor class. Exited from method%s' % e)
+            raise Exception()
+
+    def scale_numerical_columns(self, data):
+        self.logger_object.log(self.file_object,
+                               'Entered the scale_numerical_columns method of the Preprocessor class')
+        self.data = data
+
+        try:
+            self.num_df = self.data.select_dtypes(include=['int64']).copy()
+            self.scaler = StandardScaler()
+            self.scaled_data = self.scaler.fit_transform(self.num_df)
+            self.scaled_num_df = pd.DataFrame(data=self.scaled_data, columns=self.num_df.columns)
+
+            self.logger_object.log(self.file_object,
+                                   'scaling for numerical values successful. Exited the scale_numerical_columns method of the Preprocessor class')
+            return self.scaled_num_df
+
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occured in scale_numerical_columns method of the Preprocessor class. Exited method %s' % e)
+            raise e
+
+    def encode_categorical_columns(self, data):
+        self.logger_object.log(self.file_object,
+                               'Entered the encode_categorical_columns method of the Preprocessor class')
+
+        try:
+            self.cat_df = data.select_dtypes(include=['object']).copy()
+            for col in self.cat_df.columns:
+                self.cat_df = pd.get_dummies(self.cat_df, columns=[col], prefix=[col], drop_first=True)
+
+            self.logger_object.log(self.file_object,
+                                   'encoding for categorical values successful. Exited the encode_categorical_columns method of the Preprocessor class')
+            return self.cat_df
+
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occurred in encode_categorical_columns method of the Preprocessor class. Exited method %s' % e)
+            raise e
+
+    def handle_imbalanced_data(self, x, y):
+        self.logger_object.log(self.file_object,
+                               'Entered the handle_imbalanced_dataset method of the Preprocessor class')
+
+        try:
+            self.rdsmple = RandomOverSampler()
+            self.x_sampled, self.y_sampled = self.rdsmple.fit_sample(x, y)
+            self.logger_object.log(self.file_object,
+                                   'dataset balancing successful. Exited the handle_imbalanced_dataset method of the Preprocessor class')
+            return self.x_sampled, self.y_sampled
+
+        except Exception as e:
+            self.logger_object.log(self.file_object,
+                                   'Exception occured in handle_imbalanced_dataset method of the Preprocessor class. Exited method %s' % e))
+            raise e
